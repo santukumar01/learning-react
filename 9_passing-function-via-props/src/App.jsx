5262897c0482882d57fac29b1fde36180a1607c7
santukumar01 <santu13102000@gmail.com>
2024-05-01 14:38:04 +0530

    chapter-9

--- 9_passing-function-via-props/src/App.jsx
+++ 9_passing-function-via-props/src/App.jsx
@@ -1,12 +1,49 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 
+import AppHeader from "./component/AppHeader";
+import Container from "./component/Container";
+import Input from "./component/Input";
+import ItemList from "./component/ItemList";
+import ErrorMsg from "./component/ErrorMsg";
 function App() {
   // both data and function are sending from parent  tro child
+  // let foodArr = ["Dal", "Chawal", "ghee"];
+  let [foodArr, SetFoodArr] = useState(["Dal", "Chawal", "ghee"]);
+  let [text, setText] = useState("");
+
+  let handleOnChange = (event) => {
+    if (event.key === "Enter") {
+      // console.log(event);
+      // let newFood = event.target.value;
+      setText(event.target.value);
+      event.target.value = "";
+      console.log("ytou entered", text);
+      // console.log(text);
+      // console.log(text);
+      let newArray = [...foodArr, text];
+
+      // console.log(newArray);
+      SetFoodArr(newArray);
+    }
+  };
+
+  // const handleClick = () => {
+  //   // console.log(text);
+  //   let newArray = [...foodArr, text];
+
+  //   // console.log(newArray);
+  //   SetFoodArr(newArray);
+  // };
 
   return (
-    <>
-      <p>I am test</p>
-    </>
+    <Container>
+      <AppHeader />
+      <Input handleOnChange={handleOnChange} />
+      {/* {text} */}
+      <ErrorMsg foodArr={foodArr} />
+      <ItemList foodArr={foodArr} />
+    </Container>
   );
 }
 
---
+++ 9_passing-function-via-props/src/component/AppHeader.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AppHeader = () => {
+  return <h2>Healthy Food</h2>;
+};
+
+export default AppHeader;
