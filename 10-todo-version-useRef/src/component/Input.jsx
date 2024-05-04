242f94ea3a08e883bb035189691309b837a61e08
santukumar01 <santu13102000@gmail.com>
2024-05-04 15:02:20 +0530

    todo-ref-useEffect

--- 10-todo-version-useRef/src/component/Input.jsx
+++ 10-todo-version-useRef/src/component/Input.jsx
@@ -1,39 +1,45 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { IoMdAdd } from "react-icons/io";
 
 import styles from "./Input.module.css";
+// useing ref
+const Input = ({ handleAdd }) => {
+  // const [inputTodo, setInputTodo] = useState("");
+  // const [inputDate, setinputDate] = useState("");
 
-const Input = ({
-  handleAdd,
-  inputTodo,
-  inputDate,
-  setInputTodo,
-  setinputDate,
-}) => {
-  const handleinputTodo = (event) => {
-    setInputTodo(event.target.value);
-    // console.log(event.target.value);
+  const inputTodoName = useRef("");
+  const inputTodoDate = useRef("");
+
+  const handleAddbtnClicked = (event) => {
+    event.preventDefault();
+    console.log(
+      `${inputTodoName.current.value} with time ${inputTodoDate.current.value}`
+    );
+
+    const todoName = inputTodoName.current.value;
+    const todoDate = inputTodoDate.current.value;
+    // console.log("todonae", todoName)  ;  // this gives element
+
+    handleAdd(todoName, todoDate);
+    inputTodoName.current.value = "";
+    inputTodoDate.current.value = "";
   };
 
-  const handleInputDate = (event) => {
-    setinputDate(event.target.value);
-    // console.log(event.target.value);
-  };
   //  Using forms
   return (
     <div className="container text-center">
       {/* action attribute tells us that where it is going to  be submit */}
-      <form className={`${styles.inputRow} row`} onSubmit={handleAdd}>
+      <form
+        className={`${styles.inputRow} row`}
+        onSubmit={(event) =>
+          handleAddbtnClicked(event, inputTodoName, inputTodoDate)
+        }
+      >
         <div className="col-6">
-          <input
-            type="text"
-            placeholder="Enter todo"
-            value={inputTodo}
-            onChange={handleinputTodo}
-          />
+          <input type="text" placeholder="Enter todo" ref={inputTodoName} />
         </div>
         <div className="col-4">
-          <input type="date" onChange={handleInputDate} value={inputDate} />
+          <input type="date" ref={inputTodoDate} />
         </div>
         <div className="col-2">
           {/* if not given any type then it is of type submit */}
