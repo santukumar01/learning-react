2fd0d738e940e9f1285335ee2031665f7555d9fb
santukumar01 <santu13102000@gmail.com>
2024-05-04 15:37:28 +0530

    functional-update-from-pre-val

--- 10-todo-version-useRef/src/App.jsx
+++ 10-todo-version-useRef/src/App.jsx
@@ -5,6 +5,8 @@ import Appheader from "./component/Appheader";
 import Input from "./component/Input";
 import ListItem from "./component/ListItem";
 
+// react works async
+
 function App() {
   const [todo, setTodo] = useState([]);
 
@@ -13,10 +15,18 @@ function App() {
       todo: inputTodo,
       date: inputDate,
     };
-    let newTodoArr = [...todo, newObj];
-    setTodo(newTodoArr);
+    // in spread op all elemet of prevois aar copy to new element and then  new elemt added
+    // if spread is not there then  full arr ewent inside the new arr like only one elemet
+    // let newTodoArr = [...todo, newObj];
+    // setTodo(newTodoArr);
 
-    console.log(todo);
+    //updating from previous state function update
+
+    setTodo((curtodo) => {
+      // console.log(curtodo);
+      const newTodoItems = [...curtodo, newObj];
+      return newTodoItems;
+    });
   };
 
   const handleDelete = (curtodo) => {
